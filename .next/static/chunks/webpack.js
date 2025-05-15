6f98ca59a16e35cb89e8c9ae7f4abf9336a5fc24
sandunnishan <72539034+sandunnishan@users.noreply.github.com>
2025-05-15 23:07:51 +0300

    Add Article Management Tab

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3837db964ee55b2c"; }
+/******/ 		__webpack_require__.h = function() { return "a22e9171345b8789"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
